60b0643c63a4e751785855d0bf0b59acf6eb27e5
BijanProgrammer <bijaneisapour@gmail.com>
2025-03-23 05:24:26 +0330

    [REFACTOR] useTracer to handle multiple tracers

--- src/hooks/use-tracer.hook.ts
+++ src/hooks/use-tracer.hook.ts
@@ -1,19 +1,28 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import cloneDeep from "lodash-es/cloneDeep";
 
+import { TracerContext } from "@/context/tracer.context.ts";
+
 import { TracerItem } from "@/items/tracer.item.ts";
 
-export function useTracer<TItem extends TracerItem>() {
-  const [items, setItems] = useState<TItem[]>([]);
+export function useTracer<TTypes extends TracerItem[]>() {
+  const { changeStep, setTotalSteps } = useContext(TracerContext);
 
-  const trace = (item: TItem): void => {
-    const clone = cloneDeep(item);
+  const [items, setItems] = useState<TTypes[]>([]);
+
+  const trace = (items: TTypes): void => {
+    const clone = cloneDeep(items);
     setItems((old) => [...old, clone]);
+
+    setTotalSteps((old) => old + 1);
   };
 
   const reset = (): void => {
     setItems([]);
+
+    changeStep(0);
+    setTotalSteps(0);
   };
 
   return [items, trace, reset] as const;
--- src/problems/125/problem125.tsx
+++ src/problems/125/problem125.tsx
@@ -1,11 +1,9 @@
-import { ReactElement, useContext, useEffect, useState } from "react";
+import { ReactElement, useEffect, useState } from "react";
 
 import BoardComponent from "@/components/board/board.component.tsx";
 import { ButtonComponent } from "@/components/button/button.component.tsx";
 import FormComponent from "@/components/form/form.component.tsx";
 import NormalInputComponent from "@/components/normal-input/normal-input.component.tsx";
-
-import { TracerContext } from "@/context/tracer.context.ts";
 
 import { useTracer } from "@/hooks/use-tracer.hook.ts";
 
@@ -18,10 +16,7 @@ import LogTracer from "@/tracers/log/log.tracer.tsx";
 import styles from "./problem125.module.css";
 
 export default function Problem125(): ReactElement {
-  const { changeStep, setTotalSteps } = useContext(TracerContext);
-
-  const [arrayItems, traceArray, resetArray] = useTracer<ArrayTracerItem>();
-  const [logItems, traceLog, resetLog] = useTracer<LogTracerItem>();
+  const [items, trace, reset] = useTracer<[LogTracerItem, ArrayTracerItem]>();
 
   const [phrase, setPhrase] = useState<string>("radar");
 
@@ -35,7 +30,7 @@ export default function Problem125(): ReactElement {
         color: "default",
       }));
 
-    trace({ message: "Start", elements });
+    trace([{ message: "Start" }, { elements }]);
 
     let left = 0;
     let right = elements.length - 1;
@@ -48,25 +43,29 @@ export default function Problem125(): ReactElement {
 
       elements[left].color = "primary";
       elements[right].color = "primary";
-      trace({
-        message: `Is ${elements[left].value} equal to ${elements[right].value}?`,
-        elements,
-        pointers,
-      });
+      trace([
+        {
+          message: `Is ${elements[left].value} equal to ${elements[right].value}?`,
+        },
+        { elements, pointers },
+      ]);
 
       if (elements[left].value !== elements[right].value) {
         elements[left].color = "danger";
         elements[right].color = "danger";
-        trace({ message: `No`, elements, pointers });
+        trace([{ message: `No` }, { elements, pointers }]);
 
-        trace({ message: "Phrase is not palindrome", elements, pointers });
+        trace([
+          { message: "Phrase is not palindrome" },
+          { elements, pointers },
+        ]);
 
         return;
       }
 
       elements[left].color = "success";
       elements[right].color = "success";
-      trace({ message: `Yes`, elements, pointers });
+      trace([{ message: `Yes` }, { elements, pointers }]);
 
       elements[left].color = "disabled";
       elements[right].color = "disabled";
@@ -75,30 +74,9 @@ export default function Problem125(): ReactElement {
       right--;
     }
 
-    trace({ message: "Phrase is palindrome", elements });
+    trace([{ message: "Phrase is palindrome" }, { elements }]);
 
     return;
-  };
-
-  const reset = (): void => {
-    resetArray();
-    resetLog();
-    changeStep(0);
-    setTotalSteps(0);
-  };
-
-  const trace = ({
-    message,
-    elements,
-    pointers,
-  }: {
-    message: LogTracerItem["message"];
-    elements: ArrayTracerItem["elements"];
-    pointers?: ArrayTracerItem["pointers"];
-  }): void => {
-    traceArray({ elements, pointers });
-    traceLog({ message });
-    setTotalSteps((old) => old + 1);
   };
 
   useEffect(() => {
@@ -118,8 +96,8 @@ export default function Problem125(): ReactElement {
         <ButtonComponent variant="primary">Solve</ButtonComponent>
       </FormComponent>
       <BoardComponent>
-        <ArrayTracer items={arrayItems} />
-        <LogTracer items={logItems} />
+        <ArrayTracer items={items.map((x) => x[1])} />
+        <LogTracer items={items.map((x) => x[0])} />
       </BoardComponent>
     </div>
   );
